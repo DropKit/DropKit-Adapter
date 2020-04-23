62a670c52d4dac830d829522e59a90d5b2d35b9e
junwei0117 <v860117@gmail.com>
2020-04-24 05:41:11 +0800

    chore(API): Revise API response status code

--- services/response.go
+++ services/response.go
@@ -5,9 +5,9 @@ import (
 	"net/http"
 )
 
-func ResponseWithJson(w http.ResponseWriter, code int, payload interface{}) {
+func ResponseWithJson(w http.ResponseWriter, payload interface{}) {
 	response, _ := json.Marshal(payload)
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
+	w.WriteHeader(http.StatusOK)
 	w.Write(response)
 }
