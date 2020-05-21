ece5c653b787b1a72c5d4f3648bd5add7d464274
yinghan <locke2833@gmail.com>
2020-05-22 03:53:26 +0800

    test(parser): Implement tests of parser for INSERT query

---
+++ package/parser/columns/insert_test.go
@@ -0,0 +1,27 @@
+package parser
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetInsertColumns(t *testing.T) {
+	var tests = []struct {
+		testName      string
+		sqlCommand    string
+		expectColumns []string
+	}{
+		{"Case", "INSERT INTO t (QWERTYUIOPASDFGHJKLZXCVBNM, qwertyuiopasdfghjklzxcvbnm) VALUES(`aaa`,`bbb`)",
+			[]string{"QWERTYUIOPASDFGHJKLZXCVBNM", "qwertyuiopasdfghjklzxcvbnm"}},
+		{"Number", "INSERT INTO t (num1234567890) VALUES (1)", []string{"num1234567890"}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			result, _ := GetInsertColumns(tt.sqlCommand)
+			if !reflect.DeepEqual(tt.expectColumns, result) {
+				t.Errorf("got %v, expect %v", result, tt.expectColumns)
+			}
+		})
+	}
+}
